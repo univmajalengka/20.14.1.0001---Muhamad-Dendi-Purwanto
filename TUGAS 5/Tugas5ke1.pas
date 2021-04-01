0e1816cf60457e4ad623ed6a461ab2048f3b6d29
muhamaddendipurwanto <dendipurwanto72@gmail.com>
2021-04-01 08:25:48 +0700

    Upload Tugas 5

---
+++ TUGAS 5/Tugas5ke1.pas
@@ -0,0 +1,30 @@
+(*
+Nama             : Muhamad Dendi Purwanto
+NPM              : 20.14.1.0001
+Nama Program     : Tugas5ke1.pas
+*)
+
+program fungsibilangangenapganjil;
+uses crt;
+
+function bilangan (a, b : integer) : integer;
+begin
+if a mod b = 0 then
+writeln('Bilangan ini adalah bilangan genap')
+else
+writeln('bilangan ini adalah bilangan ganjil');
+end;
+
+var
+x : integer;
+begin
+clrscr;
+writeln('-------------------------------------------');
+writeln('Fungsi Bilangan Genap Dan Ganjil');
+writeln('-------------------------------------------');
+write('Masukan Bilangan   :   '); readln(x);
+writeln(bilangan(x,2));
+
+readln;
+
+end.
---
+++ TUGAS 5/program_aritmatika.pas
@@ -0,0 +1,40 @@
+(*
+Nama            : Muhamad Dendi Purwanto
+NPM             : 20.14.1.0001
+Nama Program    : program_aritmatika.pas
+*)
+
+
+program aritmatika;
+uses crt;
+
+var
+a : integer;
+b : integer;
+hasilFX : integer;
+hasilFXY : integer;
+
+function FX (x : integer) : integer;
+begin
+FX := x*x + 4*x - 5 ;
+end;
+
+function FXY (x ,y : integer) : integer;
+begin
+FXY := x*x + 2*x*y + y*y ;
+end;
+
+begin
+clrscr;
+writeln('-----------------------------------------');
+write('Masukan Nilai X   :  ' ); readln(a);
+write('Masukan Nilai Y   :  ' ); readln(b);
+
+hasilFX := FX(a);
+hasilFXY := FXY(a, b);
+
+writeln('Hasil FX  =  ', hasilFX);
+writeln('Hasil FXY =  ', hasilFXY);
+
+readln;
+end.
